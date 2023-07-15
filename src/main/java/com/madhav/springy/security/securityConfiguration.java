f7da76c7794751f85f324568ec5c1c5605984bef
Madhav Moole <madhav.moole99@gmail.com>
2023-07-15 21:52:52 +0530

    basic authentication using spring security starter

---
+++ src/main/java/com/madhav/springy/security/securityConfiguration.java
@@ -0,0 +1,47 @@
+package com.madhav.springy.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class securityConfiguration {
+
+	//This is simply non-production way of testing spring security , do not use it for actual projects
+	@Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN", "USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.requestMatchers("/user").hasRole("USER");
+                    auth.requestMatchers("/admin").hasRole("ADMIN");
+                })
+                .formLogin(withDefaults())
+                .build();
+    }
+
+}
